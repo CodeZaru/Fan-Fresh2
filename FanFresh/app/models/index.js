a050271f48e37bf427ff232fa4f2c1deff44d789
CodeZaru <teamsullivank@gmail.com>
2017-12-08 16:47:45 -0500

    tweak for heroku

--- FanFresh/app/models/index.js
+++ FanFresh/app/models/index.js
@@ -5,10 +5,17 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..','..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+//var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
  
- 
+ //added for heroku begin
+ if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+ //added for heroku end
+
 fs
     .readdirSync(__dirname)
     .filter(function(file) {

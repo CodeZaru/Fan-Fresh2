77f6e71efb15e572653fc8d2cb6d180e5fa4b8b3
CodeZaru <teamsullivank@gmail.com>
2017-12-09 03:15:26 -0500

    tweaks for heroku

--- FanFresh/app/models/index.js
+++ FanFresh/app/models/index.js
@@ -2,36 +2,43 @@
  
 var fs = require("fs");
 var path = require("path");
-var Sequelize = require("sequelize");
+//var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..','..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
- 
+var HEROKU_POSTGRESQL_BRONZE_URL = "postgres://iykonsfagnlfvl:63d5c6e91fb2c7732759ea13c5e9928d9cf8f541a4d39e848d5b1a8f82f7c799@ec2-54-221-221-153.compute-1.amazonaws.com:5432/db6s5gvfqdu4k1" 
  //added for heroku begin
- var sequelize = require('../index').connect();
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null
 
-if (sequelize)
-{
-    sequelize.authenticate().then( function() {
-        var config = sequelize.connectionManager.config;
-        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
-        
-        sequelize.query('SELECT 1+1 as test').then( function(res) {
-            
-            console.log('1+1='+res[0].test);
-            
-        });
-        
-    }).catch( function(err) {
-        var config = sequelize.connectionManager.config;
-        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
-    });
-}
-else
-{
-    console.log('No environnement variable found.');
-}
+  if (process.env.NODE_ENV) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize(config.database, config.username, config.password, config)
+  }
+
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    User:      sequelize.import(__dirname + '/user') 
+    // add your other models here
+  }
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
+
 
 
  /*
@@ -67,7 +74,10 @@ Object.keys(db).forEach(function(modelName) {
 });
  
  
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+//db.sequelize = sequelize;
+//db.Sequelize = Sequelize;
  
-module.exports = db;
+//module.exports = db;
+}
+
+module.exports = global.db
